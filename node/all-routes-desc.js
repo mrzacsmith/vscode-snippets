ad1308d444b5da0526980ce9a278a81e6a364bf8
Zac Smith <zrs3141592@gmail.com>
2020-10-26 20:03:55 -0600

    all routes description

---
+++ node/all-routes-desc.js
@@ -0,0 +1,6 @@
+"All Route comments": {
+  "scope": "javascript",
+  "prefix": "//a",
+  "body": "// @desc\t\tTest endpoint is working\n// @route\t\t/test\n\n// @desc\t\tGet all $1s\n// @route\t\tGET /\n\n// @desc\t\tGet a $1 by id\n// @route\t\tGET /:id\n\n // @desc\t\tAdd a new $1\n// @route\t\tPOST /\n\n// @desc\t\tUpdate a $1 by id\n // @route\t\tPATCH /:id\n\n// @desc\t\tRemove a $1 by id\n// @route\t\tDELETE /:id\n",
+  "description": "comments for // @desc // @route"
+}
